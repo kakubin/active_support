c9b9f2ddb97be4a06a2d1c6709b46dacf216b518
mrbigass <wetsand.wfs@gmail.com>
2021-05-16 12:59:12 +0900

    initial

---
+++ lib/active_support.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'active_support/version'
+
+module ActiveSupport
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/active_support/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ActiveSupport
+  VERSION = '0.1.0'
+end
